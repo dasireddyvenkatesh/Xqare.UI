f5f97e0f5659abc428c480c1fdaab0c2ba43955b
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2026-04-21 13:19:55 +0530

    Home Page Main Changes

---
+++ Xqare/wwwroot/js/scrollable.js
@@ -0,0 +1,9 @@
+﻿window.headerScrollHandler = (dotnetHelper) => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 10) {
+            dotnetHelper.invokeMethodAsync("OnScroll", true);
+        } else {
+            dotnetHelper.invokeMethodAsync("OnScroll", false);
+        }
+    });
+};
