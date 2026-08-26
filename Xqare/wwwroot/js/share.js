ddb4a4fb4875ee86eacfae4a2a6b075fc8034e7c
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2026-08-26 15:04:58 +0530

    All Links Fixes

---
+++ Xqare/wwwroot/js/share.js
@@ -0,0 +1,18 @@
+﻿window.shareArticle = async function (url, title) {
+    if (navigator.share) {
+        try {
+            await navigator.share({
+                title: title,
+                url: url
+            });
+        } catch (error) {
+            // User cancelled the share dialog.
+        }
+    } else {
+        try {
+            await navigator.clipboard.writeText(url);
+        } catch (error) {
+            console.error("Could not copy URL:", error);
+        }
+    }
+};
